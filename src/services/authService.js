028879a42ff594850230642b157f015bc051bbb2
Dev <dev@round-x.com>
2022-07-28 06:32:21 +0900

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -37,9 +37,6 @@ class AuthService {
             if (error.code === 'auth/wrong-password') {
                 let notification = i18n.t("NOTIFICATION_SERVICE_INVALID_LOGIN");
                 return unauthorized(notification);
-            } else if (error.response.data.code === 401) {
-                let notification = i18n.t("NOTIFICATION_SERVICE_INVALID_LOGIN");
-                return unauthorized(notification);
             } else if (error.code === "auth/user-not-found") {
                 let notification = i18n.t("NOTIFICATION_SERVICE_INVALID_LOGIN");
                 return unauthorized(notification);
